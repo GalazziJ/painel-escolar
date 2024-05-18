51a7713ec47bc15d07769de9d917a8e898edef52
Jamille Galazi <jamillegalazidf7@gmail.com>
2024-05-17 21:42:23 -0300

    Criando aplicação express

---
+++ Loja_Parafuso/server/src/App.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const UsuarioController = require("./controllers/UsuarioController")
+
+
+const port = 5000
+
+// permitindo leitura de JSON
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+// rotas para o CRUD
+app.post('/usuarios', UsuarioController.create)
+app.get('/usuarios', UsuarioController.read)
+app.put('/usuarios/:id_usuario', UsuarioController.update)
+app.delete('/usuarios/:id_usuario', UsuarioController.delete)
---
+++ Loja_Parafuso/server/src/models/Usuarios.js
@@ -0,0 +1,73 @@
+
+const mysql = require("mysql2")
+const config = require("../Config")
+
+class Usuario {
+
+  constructor() {
+    this.conexao = mysql.createConnection(config.db)
+  }
+
+  inserirUsuario(nome, login, senha) {
+    let sql = `INSERT INTO usuarios (nome, login, senha) VALUES
+    ("${nome}", "${login}", "${senha}");`
+
+    return new Promise((resolve, reject) => {
+      this.conexao.query(sql, function (erro, retorno) {
+        if (erro) {
+          reject([400, erro])
+        } else {
+          resolve([201, "Usuário inserido"])
+        }
+      })
+    })
+  }
+
+  mostrarUsuarios() {
+    let sql = "SELECT * FROM usuarios"
+
+    return new Promise((resolve, reject) => {
+      this.conexao.query(sql, function (erro, retorno) {
+        if (erro) {
+          reject([400, erro])
+        } else {
+          resolve([200, retorno])
+        }
+      })
+    })
+  }
+
+  atualizarUsuario(id_usuario, nome, login, senha) {
+    let sql = `UPDATE usuarios SET nome="${nome}", login="${login}", senha="${senha}" WHERE idusuario="${id_usuario}";`
+    return new Promise((resolve, reject) => {
+      this.conexao.query(sql, function (erro, retorno) {
+        if (erro) {
+          reject([400, erro])
+        } else {
+          resolve([200, retorno])
+        }
+      })
+    })
+  }
+
+  deletarUsuario(id_usuario){
+    let sql = `DELETE FROM usuarios WHERE idusuario="${id_usuario}";`
+
+    return new Promise((resolve, reject) => {
+      this.conexao.query(sql, function (erro, retorno) {
+        if (erro) {
+          reject([400, erro])
+        } else {
+          resolve([200, retorno])
+        }
+        if (retorno["affectedRows"]>0) {
+          resolve([200, "Usuário deletado"])
+        } else {
+          resolve([404, "Usuário não encontrado"])
+        }
+      })
+    })
+  }
+}
+
+module.exports = new Usuario()
